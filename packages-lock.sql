fafd77fe2f09d8899b40246c5509e6d19fec7bcc
vamsik2105 <vamsikrish12103@gmail.com>
2025-09-26 07:36:22 +0530

    commiting after adding logs code 2nd try

---
+++ packages-lock.sql
@@ -0,0 +1,11 @@
+packages:
+  - name: dbt_utils
+    package: dbt-labs/dbt_utils
+    version: 1.1.1
+  - name: dbt_expectations
+    package: calogica/dbt_expectations
+    version: 0.10.1
+  - name: dbt_date
+    package: calogica/dbt_date
+    version: 0.10.1
+sha1_hash: 996c1dafdff1ba521f361aca0883b2d64181bdf8
