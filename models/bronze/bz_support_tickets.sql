18c56ca3d46881cda54dc11a6f94b9ca236ec238
vamsik2105 <vamsikrish12103@gmail.com>
2025-09-29 15:59:46 +0530

    Initial commit for Zoom Customer Analytics bronze layer transformation

--- models/bronze/bz_support_tickets.sql
+++ models/bronze/bz_support_tickets.sql
@@ -1,7 +1,5 @@
 {{config(
-  materialized = 'table',
-  pre_hook="{{ log_model_start('bz_support_tickets') }}",
-  post_hook="{{ log_model_completion('bz_support_tickets', adapter.get_relation(this.database, this.schema, this.name).get_row_count()) }}"
+  materialized = 'table'
 )}}
 
 -- Transform raw support tickets data to bronze layer
