7d09aee5c38ccdff52499965ff330d16f26c4977
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 18:23:05 +0530

    Direct SQL references to raw tables

--- models/bronze/bz_billing_events.sql
+++ models/bronze/bz_billing_events.sql
@@ -1,12 +1,6 @@
 {{config(
   materialized = 'table',
-  schema = 'bronze',
-  pre_hook=[
-    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('billing_events', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
-  ],
-  post_hook=[
-    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('billing_events', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'billing_events' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
-  ]
+  schema = 'bronze'
 )}}
 
 SELECT
@@ -18,4 +12,4 @@ SELECT
   load_timestamp,
   update_timestamp,
   'ZOOM_PLATFORM' as source_system
-FROM {{ source('raw', 'billing_events') }}
+FROM raw.billing_events
