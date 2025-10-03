4c187765b942b423cf041c42c48118a302571280
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 16:01:02 +0530

    Simplify bz_users model for testing

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,11 +1,5 @@
 {{config(
-  materialized = 'incremental',
-  pre_hook=[
-    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('users', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
-  ],
-  post_hook=[
-    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('users', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'users' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
-  ]
+  materialized = 'table'
 )}}
 
 SELECT
