f6bcab1d88bb3bbde34c9a78c8cfd02453f86ae6
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 18:23:04 +0530

    Direct SQL references to raw tables

--- models/bronze/bz_licenses.sql
+++ models/bronze/bz_licenses.sql
@@ -1,12 +1,6 @@
 {{config(
   materialized = 'table',
-  schema = 'bronze',
-  pre_hook=[
-    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('licenses', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
-  ],
-  post_hook=[
-    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('licenses', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'licenses' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
-  ]
+  schema = 'bronze'
 )}}
 
 SELECT
@@ -18,4 +12,4 @@ SELECT
   load_timestamp,
   update_timestamp,
   'ZOOM_PLATFORM' as source_system
-FROM {{ source('raw', 'licenses') }}
+FROM raw.licenses
