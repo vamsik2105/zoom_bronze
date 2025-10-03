520524d5800510367d85cb5ab14fd45e28593b4e
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 15:58:31 +0530

    Update models with direct pre-hooks and post-hooks

--- models/bronze/bz_participants.sql
+++ models/bronze/bz_participants.sql
@@ -1,16 +1,10 @@
 {{config(
   materialized = 'incremental',
   pre_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-      VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')
-    {% endif %}"
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
   ],
   post_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-      VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'participants' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')
-    {% endif %}"
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'participants' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
   ]
 )}}
 
