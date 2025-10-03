59342b8efb83fb141d8d99648b9e58553d65401e
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 16:02:35 +0530

    Update all bronze models with simplified configuration

--- models/bronze/bz_licenses.sql
+++ models/bronze/bz_licenses.sql
@@ -1,17 +1,5 @@
 {{config(
-  materialized = 'incremental',
-  pre_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-      VALUES ('licenses', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')
-    {% endif %}"
-  ],
-  post_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-      VALUES ('licenses', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'licenses' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')
-    {% endif %}"
-  ]
+  materialized = 'table'
 )}}
 
 SELECT
