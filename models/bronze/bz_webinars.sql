25e7cd9da0ae2e35889f06d4063f246b32ce3c7a
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 16:02:34 +0530

    Update all bronze models with simplified configuration

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,17 +1,5 @@
 {{config(
-  materialized = 'incremental',
-  pre_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-      VALUES ('webinars', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')
-    {% endif %}"
-  ],
-  post_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-      VALUES ('webinars', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'webinars' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')
-    {% endif %}"
-  ]
+  materialized = 'table'
 )}}
 
 SELECT
