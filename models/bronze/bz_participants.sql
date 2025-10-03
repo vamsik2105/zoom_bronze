52f610b912711232b5fc123bc5b51feb886b6311
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 15:53:05 +0530

    Initial commit for Zoom Customer Analytics bronze layer

---
+++ models/bronze/bz_participants.sql
@@ -0,0 +1,26 @@
+{{config(
+  materialized = 'incremental',
+  pre_hook=[
+    "{% if target.table != 'bz_audit_log' %}
+      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
+      VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')
+    {% endif %}"
+  ],
+  post_hook=[
+    "{% if target.table != 'bz_audit_log' %}
+      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+      VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'participants' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')
+    {% endif %}"
+  ]
+)}}
+
+SELECT
+  participant_id,
+  meeting_id,
+  user_id,
+  join_time,
+  leave_time,
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_TIMESTAMP() AS update_timestamp,
+  'ZOOM_PLATFORM' AS source_system
+FROM {{ source('raw', 'participants') }}
