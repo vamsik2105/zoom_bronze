91495307847c49ccefb7552fb66eca9fde781042
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 16:05:26 +0530

    Final version of bronze layer models with proper documentation and configuration

--- models/bronze/bz_participants.sql
+++ models/bronze/bz_participants.sql
@@ -1,7 +1,14 @@
 {{config(
-  materialized = 'table'
+  materialized = 'table',
+  pre_hook=[
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
+  ],
+  post_hook=[
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('participants', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'participants' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
+  ]
 )}}
 
+-- Extract participants data from raw layer to bronze layer
 SELECT
   participant_id,
   meeting_id,
