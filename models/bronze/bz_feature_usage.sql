acc17fc6414a1e19c8a0c2332966718f6b69e97b
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 16:05:26 +0530

    Final version of bronze layer models with proper documentation and configuration

--- models/bronze/bz_feature_usage.sql
+++ models/bronze/bz_feature_usage.sql
@@ -1,7 +1,14 @@
 {{config(
-  materialized = 'table'
+  materialized = 'table',
+  pre_hook=[
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('feature_usage', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
+  ],
+  post_hook=[
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('feature_usage', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'feature_usage' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
+  ]
 )}}
 
+-- Extract feature usage data from raw layer to bronze layer
 SELECT
   usage_id,
   meeting_id,
