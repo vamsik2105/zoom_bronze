2f6a5f42efd0fc14de2f97d7e0ae2244b5e9f098
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 15:57:02 +0530

    Move macros to proper directory and update bz_users model

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -2,14 +2,12 @@
   materialized = 'incremental',
   pre_hook=[
     "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-      VALUES ('users', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')
+      {{ log_audit_start('users') }}
     {% endif %}"
   ],
   post_hook=[
     "{% if target.table != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-      VALUES ('users', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'users' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')
+      {{ log_audit_end('users') }}
     {% endif %}"
   ]
 )}}
