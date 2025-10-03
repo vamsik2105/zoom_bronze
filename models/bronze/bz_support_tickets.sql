f471f08bbf1fd963218ceabe8ea6e62d31c1634d
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 16:05:27 +0530

    Final version of bronze layer models with proper documentation and configuration

--- models/bronze/bz_support_tickets.sql
+++ models/bronze/bz_support_tickets.sql
@@ -1,7 +1,14 @@
 {{config(
-  materialized = 'table'
+  materialized = 'table',
+  pre_hook=[
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('support_tickets', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
+  ],
+  post_hook=[
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('support_tickets', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'support_tickets' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
+  ]
 )}}
 
+-- Extract support tickets data from raw layer to bronze layer
 SELECT
   ticket_id,
   user_id,
