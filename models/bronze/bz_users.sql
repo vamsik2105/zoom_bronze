a4cf6783ec773fbe96d1a99b12c3e126235cf259
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 15:58:29 +0530

    Update models with direct pre-hooks and post-hooks

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,14 +1,10 @@
 {{config(
   materialized = 'incremental',
   pre_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      {{ log_audit_start('users') }}
-    {% endif %}"
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status) VALUES ('users', CURRENT_TIMESTAMP(), CURRENT_USER(), 'STARTED')"
   ],
   post_hook=[
-    "{% if target.table != 'bz_audit_log' %}
-      {{ log_audit_end('users') }}
-    {% endif %}"
+    "INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('users', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'users' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
   ]
 )}}
 
