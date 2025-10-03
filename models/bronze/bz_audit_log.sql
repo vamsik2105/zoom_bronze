0dea237623bd018e6f18410027aac47e8b9b6dd6
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-03 15:53:03 +0530

    Initial commit for Zoom Customer Analytics bronze layer

---
+++ models/bronze/bz_audit_log.sql
@@ -0,0 +1,14 @@
+{{config(
+  materialized = 'incremental',
+  unique_key = 'record_id'
+)}}
+
+-- Create the audit log table if it doesn't exist
+SELECT
+  NULL AS record_id,  -- This will be auto-incremented
+  NULL AS source_table,
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_USER() AS processed_by,
+  NULL AS processing_time,
+  NULL AS status
+WHERE 1=0  -- Empty initial table
