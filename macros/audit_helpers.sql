d3de2262484cb1573e5e7c32d213e1b6edbf2654
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 07:30:51 +0530

    Automated DBT Sync from AAVA

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,26 +1,28 @@
 {% macro log_audit_start(model_name) %}
-  {% if execute %}
-    {% if model_name != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
+  {% if execute and model_name != 'bz_audit_log' %}
+    {% set query %}
+      INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, status)
       SELECT 
         '{{ model_name }}',
         CURRENT_TIMESTAMP(),
         'DBT',
         'STARTED'
-    {% endif %}
+    {% endset %}
+    {% do run_query(query) %}
   {% endif %}
 {% endmacro %}
 
 {% macro log_audit_end(model_name) %}
-  {% if execute %}
-    {% if model_name != 'bz_audit_log' %}
-      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+  {% if execute and model_name != 'bz_audit_log' %}
+    {% set query %}
+      INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status)
       SELECT 
         '{{ model_name }}',
         CURRENT_TIMESTAMP(),
         'DBT',
-        DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ model_name }}' AND status = 'STARTED'), CURRENT_TIMESTAMP()),
+        DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ target.schema }}.bz_audit_log WHERE source_table = '{{ model_name }}' AND status = 'STARTED'), CURRENT_TIMESTAMP()),
         'COMPLETED'
-    {% endif %}
+    {% endset %}
+    {% do run_query(query) %}
   {% endif %}
 {% endmacro %}
