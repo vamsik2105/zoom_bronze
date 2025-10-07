a1b56b3a13ff595cb288bb7ad20ece80fa36d6e4
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 07:29:26 +0530

    Automated DBT Sync from AAVA

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,18 +1,26 @@
-{% macro log_table_process_start(model_name) %}
-{% if model_name != 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-    SELECT '{{ model_name }}', CURRENT_TIMESTAMP(), '{{ invocation_id }}', 'STARTED';
-{% endif %}
-{% endmacro %}
-
-{% macro log_table_process_end(model_name, start_time) %}
-{% if model_name != 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    SELECT 
+{% macro log_audit_start(model_name) %}
+  {% if execute %}
+    {% if model_name != 'bz_audit_log' %}
+      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
+      SELECT 
         '{{ model_name }}',
         CURRENT_TIMESTAMP(),
-        '{{ invocation_id }}',
-        DATEDIFF('MILLISECOND', {{ start_time }}, CURRENT_TIMESTAMP()) / 1000.0,
-        'COMPLETED';
-{% endif %}
+        'DBT',
+        'STARTED'
+    {% endif %}
+  {% endif %}
+{% endmacro %}
+
+{% macro log_audit_end(model_name) %}
+  {% if execute %}
+    {% if model_name != 'bz_audit_log' %}
+      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+      SELECT 
+        '{{ model_name }}',
+        CURRENT_TIMESTAMP(),
+        'DBT',
+        DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ model_name }}' AND status = 'STARTED'), CURRENT_TIMESTAMP()),
+        'COMPLETED'
+    {% endif %}
+  {% endif %}
 {% endmacro %}
