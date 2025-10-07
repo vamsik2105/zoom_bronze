78262757df5a7ff6f20402fab34238e60f93ca43
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 07:32:13 +0530

    Automated DBT Sync from AAVA

--- models/bz_users.sql
+++ models/bz_users.sql
@@ -1,15 +1,4 @@
-{{config(
-  materialized = 'table'
-)}}
-
--- Transform raw users data to bronze layer
+-- Simple transformation for users
 SELECT
-  user_id,
-  user_name,
-  email,
-  company,
-  plan_type,
-  CURRENT_TIMESTAMP() as load_timestamp,
-  CURRENT_TIMESTAMP() as update_timestamp,
-  'ZOOM_PLATFORM' as source_system
+  *
 FROM {{ source('raw', 'users') }}
