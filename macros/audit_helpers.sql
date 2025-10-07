a61c030c1364611b10a2e6fde960570a9a554523
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 15:49:29 +0530

    Automated DBT Sync from AAVA

---
+++ macros/audit_helpers.sql
@@ -0,0 +1,30 @@
+{% macro log_audit_start(model_name) %}
+  {% if execute %}
+    {% set audit_query %}
+      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+      SELECT 
+        '{{ model_name }}',
+        CURRENT_TIMESTAMP(),
+        CURRENT_USER(),
+        0,
+        'STARTED'
+    {% endset %}
+    {% do run_query(audit_query) %}
+  {% endif %}
+{% endmacro %}
+
+{% macro log_audit_end(model_name, success) %}
+  {% if execute %}
+    {% set status = 'SUCCESS' if success else 'FAILED' %}
+    {% set audit_query %}
+      INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+      SELECT 
+        '{{ model_name }}',
+        CURRENT_TIMESTAMP(),
+        CURRENT_USER(),
+        DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ model_name }}' AND status = 'STARTED'), CURRENT_TIMESTAMP()),
+        '{{ status }}'
+    {% endset %}
+    {% do run_query(audit_query) %}
+  {% endif %}
+{% endmacro %}
