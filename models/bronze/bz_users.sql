088ef5e8b56779402aa0472c483080b2b2b8c826
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 15:57:27 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -3,8 +3,8 @@
 {{ config(
     materialized = 'table',
     tags = ['bronze'],
-    pre_hook = "{{ log_audit_start('bz_users') }}",
-    post_hook = "{{ log_audit_end('bz_users', True) }}"
+    pre_hook = "INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('bz_users', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'STARTED')",
+    post_hook = "INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('bz_users', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'SUCCESS')"
 ) }}
 
 SELECT
