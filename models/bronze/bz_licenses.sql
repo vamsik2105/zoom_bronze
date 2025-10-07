c30fd4ff532eb1797a066c098a2404da0b7b0386
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 15:49:33 +0530

    Automated DBT Sync from AAVA

---
+++ models/bronze/bz_licenses.sql
@@ -0,0 +1,21 @@
+-- Bronze layer transformation for licenses data
+
+{{ config(
+    materialized = 'table',
+    tags = ['bronze'],
+    pre_hook = "{{ log_audit_start('bz_licenses') }}",
+    post_hook = "{{ log_audit_end('bz_licenses', True) }}"
+) }}
+
+SELECT
+    -- Direct 1:1 mapping from source
+    license_id,
+    license_type,
+    assigned_to_user_id,
+    start_date,
+    end_date,
+    -- Metadata columns
+    CURRENT_TIMESTAMP() as load_timestamp,
+    CURRENT_TIMESTAMP() as update_timestamp,
+    'ZOOM_PLATFORM' as source_system
+FROM {{ source('raw', 'licenses') }}
