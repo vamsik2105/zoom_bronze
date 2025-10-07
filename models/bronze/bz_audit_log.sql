2236d3a1ff247e42ca7ea49e0ddbfeabf261f16a
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 07:24:03 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -2,13 +2,7 @@
 -- This table tracks the processing of all other models
 
 {{ config(
-    materialized = 'table',
-    pre_hook = """
-        -- No pre-hook needed for audit log as it's the first table
-    """,
-    post_hook = """
-        -- No post-hook needed for audit log
-    """
+    materialized = 'table'
 ) }}
 
 -- Initialize the audit log table if it doesn't exist
@@ -19,4 +13,4 @@ SELECT
     'SYSTEM' as processed_by,
     0 as processing_time,
     'INITIALIZED' as status
-WHERE NOT EXISTS (SELECT 1 FROM {{ this }})
+WHERE 1=0
