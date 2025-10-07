fc71101bcaa5eb4f2a916669db63515ee98f982c
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-07 15:52:39 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -2,9 +2,7 @@
 
 {{ config(
     materialized = 'table',
-    tags = ['bronze'],
-    pre_hook = "{{ log_audit_start('bz_meetings') }}",
-    post_hook = "{{ log_audit_end('bz_meetings', True) }}"
+    tags = ['bronze']
 ) }}
 
 SELECT
