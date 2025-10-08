dc3503952da94fbefbc055b755569f7db9ef2fef
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-08 18:16:20 +0530

    Automated DBT Sync from AAVA

--- macros/audit_helpers.sql
+++ macros/audit_helpers.sql
@@ -1,6 +1,6 @@
 {% macro log_audit_start(model_name) %}
   {% if model_name != 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status)
     SELECT 
       '{{ model_name }}',
       CURRENT_TIMESTAMP(),
@@ -12,24 +12,24 @@
 
 {% macro log_audit_end(model_name) %}
   {% if model_name != 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status)
     SELECT 
       '{{ model_name }}',
       CURRENT_TIMESTAMP(),
       'dbt',
-      DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ model_name }}' AND status = 'started'), CURRENT_TIMESTAMP()),
+      DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ target.schema }}.bz_audit_log WHERE source_table = '{{ model_name }}' AND status = 'started'), CURRENT_TIMESTAMP()),
       'completed'
   {% endif %}
 {% endmacro %}
 
 {% macro log_audit_error(model_name) %}
   {% if model_name != 'bz_audit_log' %}
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+    INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status)
     SELECT 
       '{{ model_name }}',
       CURRENT_TIMESTAMP(),
       'dbt',
-      DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = '{{ model_name }}' AND status = 'started'), CURRENT_TIMESTAMP()),
+      DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ target.schema }}.bz_audit_log WHERE source_table = '{{ model_name }}' AND status = 'started'), CURRENT_TIMESTAMP()),
       'error'
   {% endif %}
 {% endmacro %}
