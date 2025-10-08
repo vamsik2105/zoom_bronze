76058210549fd4ac715280da4f5d4847f666c416
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-08 18:17:11 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -6,10 +6,9 @@
 -- Create the audit log table if it doesn't exist
 -- This will be used to track the processing of all bronze models
 SELECT
-    NULL as record_id,
+    1 as record_id,
     'initial_setup' as source_table,
     CURRENT_TIMESTAMP() as load_timestamp,
     'dbt' as processed_by,
     0 as processing_time,
     'initialized' as status
-WHERE 1=0  -- Empty initial table, will be populated by pre/post hooks
