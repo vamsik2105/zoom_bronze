b6df8bf0262697e82ba09eab26c5950e742c7574
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 01:57:12 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -5,7 +5,6 @@
 
 -- Create audit log table to track processing of all models
 SELECT
-  -- Using Snowflake's AUTOINCREMENT feature for record_id
   NULL as record_id,
   'INITIAL_SETUP' as source_table,
   CURRENT_TIMESTAMP() as load_timestamp,
