ad125535baccc84c3666fe1b3da105d3b79d12d5
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 02:00:15 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -1,14 +1,12 @@
 {{config(
-  materialized = 'incremental',
-  unique_key = 'record_id'
+  materialized = 'table'
 )}}
 
 -- Create audit log table to track processing of all models
 SELECT
-  NULL as record_id,
+  1 as record_id,
   'INITIAL_SETUP' as source_table,
   CURRENT_TIMESTAMP() as load_timestamp,
   CURRENT_USER() as processed_by,
   0 as processing_time,
   'SUCCESS' as status
-WHERE FALSE  -- Don't insert any rows on initial creation
