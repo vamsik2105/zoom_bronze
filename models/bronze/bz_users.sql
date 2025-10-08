8c79304d53243b8368d30279666430765b822d7f
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 02:00:15 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,7 +1,5 @@
 {{config(
-  materialized = 'table',
-  pre_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'users', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'PROCESSING' WHERE '{{ this.name }}' != 'bz_audit_log'",
-  post_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'users', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'users' AND status = 'PROCESSING'), CURRENT_TIMESTAMP()), 'SUCCESS' WHERE '{{ this.name }}' != 'bz_audit_log'"
+  materialized = 'table'
 )}}
 
 -- Transform raw users data to bronze layer
