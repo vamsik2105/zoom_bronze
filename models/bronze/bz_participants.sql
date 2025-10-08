c96df2de080b1646378c1b8a97caa65d1f8dd5c2
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 01:54:47 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_participants.sql
+++ models/bronze/bz_participants.sql
@@ -1,17 +1,8 @@
 {{config(
-  materialized = 'table'
+  materialized = 'table',
+  pre_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'participants', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'PROCESSING' WHERE '{{ this.name }}' != 'bz_audit_log'",
+  post_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'participants', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'participants' AND status = 'PROCESSING'), CURRENT_TIMESTAMP()), 'SUCCESS' WHERE '{{ this.name }}' != 'bz_audit_log'"
 )}}
-
--- Pre-hook to log the start of processing
-{% set source_table = 'participants' %}
-{{ config(
-  pre_hook="{{ log_audit_start('" ~ source_table ~ "') }}"
-) }}
-
--- Post-hook to log the end of processing
-{{ config(
-  post_hook="{{ log_audit_end('" ~ source_table ~ "', 'SUCCESS') }}"
-) }}
 
 -- Transform raw participants data to bronze layer
 SELECT
