9a1a749d624a9553cc537273d7883cc7ab9ad28c
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 02:01:34 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_feature_usage.sql
+++ models/bronze/bz_feature_usage.sql
@@ -1,7 +1,5 @@
 {{config(
-  materialized = 'table',
-  pre_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'feature_usage', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'PROCESSING' WHERE '{{ this.name }}' != 'bz_audit_log'",
-  post_hook="INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status) SELECT 'feature_usage', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('SECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'feature_usage' AND status = 'PROCESSING'), CURRENT_TIMESTAMP()), 'SUCCESS' WHERE '{{ this.name }}' != 'bz_audit_log'"
+  materialized = 'table'
 )}}
 
 -- Transform raw feature_usage data to bronze layer
