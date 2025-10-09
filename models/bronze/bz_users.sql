d673d9cb6b8703a6aa5dfc243e8486152ef0647e
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 20:50:36 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,10 +1,5 @@
-{{ config(
-    materialized='table'
-) }}
+{{ config(materialized='table') }}
 
-SELECT 
-    *,
-    CURRENT_TIMESTAMP as load_timestamp,
-    CURRENT_TIMESTAMP as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-FROM {{ source('raw', 'users') }}
+select
+    *
+from {{ source('raw', 'users') }}
