ad9b56421ef4bd4e5e55fa26d0277333a9f9f21b
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 20:46:28 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -1,7 +1,5 @@
 {{ config(
-    materialized='table',
-    pre_hook="INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('bz_audit_log', CURRENT_TIMESTAMP, 'dbt', 0, 'STARTED') WHERE FALSE",
-    post_hook="INSERT INTO {{ target.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('bz_audit_log', CURRENT_TIMESTAMP, 'dbt', 0, 'COMPLETED')"
+    materialized='table'
 ) }}
 
 -- Bronze Audit Log Table
@@ -12,7 +10,7 @@ SELECT
     CAST('INITIAL_LOAD' AS VARCHAR(255)) as source_table,
     CURRENT_TIMESTAMP as load_timestamp,
     'dbt' as processed_by,
-    0 as processing_time,
+    CAST(0 AS NUMBER) as processing_time,
     'INITIALIZED' as status
 
 WHERE FALSE -- This ensures the table structure is created but no initial data is inserted
