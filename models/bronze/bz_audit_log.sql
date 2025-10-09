81eb249331800ee32bee7f6f0349b74dc2ad0048
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 20:47:03 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -2,15 +2,10 @@
     materialized='table'
 ) }}
 
--- Bronze Audit Log Table
--- This table tracks all bronze layer processing activities
-
 SELECT 
-    CAST(NULL AS NUMBER) AS record_id,
-    CAST('INITIAL_LOAD' AS VARCHAR(255)) as source_table,
+    1 as record_id,
+    'INITIAL_LOAD' as source_table,
     CURRENT_TIMESTAMP as load_timestamp,
     'dbt' as processed_by,
-    CAST(0 AS NUMBER) as processing_time,
+    0 as processing_time,
     'INITIALIZED' as status
-
-WHERE FALSE -- This ensures the table structure is created but no initial data is inserted
