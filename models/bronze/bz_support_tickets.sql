2ec3bd17a48b20afcc02029b9e239d6a4cd4e635
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 13:48:11 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_support_tickets.sql
+++ models/bronze/bz_support_tickets.sql
@@ -1,25 +1,8 @@
 {{config(
   materialized = 'table',
   schema = 'bronze',
-  pre_hook=["""
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-    SELECT
-      'support_tickets' AS source_table,
-      CURRENT_TIMESTAMP() AS load_timestamp,
-      CURRENT_USER() AS processed_by,
-      'STARTED' AS status
-    WHERE '{{ this.name }}' != 'bz_audit_log'
-  """],
-  post_hook=["""
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    SELECT
-      'support_tickets' AS source_table,
-      CURRENT_TIMESTAMP() AS load_timestamp,
-      CURRENT_USER() AS processed_by,
-      DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'support_tickets' AND status = 'STARTED'), CURRENT_TIMESTAMP()) AS processing_time,
-      'COMPLETED' AS status
-    WHERE '{{ this.name }}' != 'bz_audit_log'
-  """]
+  pre_hook="{{ log_table_processing_start('support_tickets') }}",
+  post_hook="{{ log_table_processing_complete('support_tickets') }}"
 )}}
 
 -- Transform raw support_tickets data to bronze layer
