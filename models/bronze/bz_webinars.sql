06302648ddd1e95f4c41c0f3925815531e4b8fed
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-09 13:48:10 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,25 +1,8 @@
 {{config(
   materialized = 'table',
   schema = 'bronze',
-  pre_hook=["""
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, status)
-    SELECT
-      'webinars' AS source_table,
-      CURRENT_TIMESTAMP() AS load_timestamp,
-      CURRENT_USER() AS processed_by,
-      'STARTED' AS status
-    WHERE '{{ this.name }}' != 'bz_audit_log'
-  """],
-  post_hook=["""
-    INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-    SELECT
-      'webinars' AS source_table,
-      CURRENT_TIMESTAMP() AS load_timestamp,
-      CURRENT_USER() AS processed_by,
-      DATEDIFF('MILLISECOND', (SELECT MAX(load_timestamp) FROM {{ ref('bz_audit_log') }} WHERE source_table = 'webinars' AND status = 'STARTED'), CURRENT_TIMESTAMP()) AS processing_time,
-      'COMPLETED' AS status
-    WHERE '{{ this.name }}' != 'bz_audit_log'
-  """]
+  pre_hook="{{ log_table_processing_start('webinars') }}",
+  post_hook="{{ log_table_processing_complete('webinars') }}"
 )}}
 
 -- Transform raw webinars data to bronze layer
