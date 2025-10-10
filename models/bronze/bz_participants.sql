132f11a2c2acb5fa72c6a684e2d8a036689ceb03
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-10 20:05:28 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_participants.sql
+++ models/bronze/bz_participants.sql
@@ -1,9 +1,11 @@
 {{ config(
-    materialized='table'
+    materialized='table',
+    pre_hook="INSERT INTO {{ this.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('bz_participants', CURRENT_TIMESTAMP(), 'dbt_bronze_layer', 0, 'STARTED')",
+    post_hook="INSERT INTO {{ this.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) VALUES ('bz_participants', CURRENT_TIMESTAMP(), 'dbt_bronze_layer', DATEDIFF('second', (SELECT MAX(load_timestamp) FROM {{ this.schema }}.bz_audit_log WHERE source_table = 'bz_participants' AND status = 'STARTED'), CURRENT_TIMESTAMP()), 'COMPLETED')"
 ) }}
 
--- Transform raw participants data to bronze layer with 1-to-1 mapping
-SELECT
+-- Bronze layer transformation for participants table
+SELECT 
     participant_id,
     meeting_id,
     user_id,
@@ -11,8 +13,5 @@ SELECT
     leave_time,
     load_timestamp,
     update_timestamp,
-    source_system,
-    CURRENT_TIMESTAMP() as bronze_created_at,
-    'dbt' as bronze_created_by
+    source_system
 FROM {{ source('raw', 'participants') }}
-WHERE participant_id IS NOT NULL
