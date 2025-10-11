860ed8e19e2036a732ce1fa8cc09692e0249546c
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-12 02:58:07 +0530

    Automated DBT Sync

--- models/bronze/audit_entries.sql
+++ models/bronze/audit_entries.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized='table',
     tags=['audit', 'bronze'],
-    post_hook="INSERT INTO {{ ref('audit_log_bz') }} (audit_id, source_layer, source_table, target_layer, target_table, load_type, load_start_time, load_end_time, record_count_loaded, status, run_id, created_by, created_at) VALUES (1, 'RAW', 'CUSTOMER', 'BRONZE', 'CUSTOMER', 'FULL', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), (SELECT COUNT(*) FROM {{ ref('bronze_customer') }}), 'SUCCESS', '{{ invocation_id }}', CURRENT_USER(), CURRENT_TIMESTAMP());"
+    post_hook="INSERT INTO {{ ref('audit_log_bz') }} (audit_id, source_layer, source_table, target_layer, target_table, load_type, load_start_time, load_end_time, record_count_loaded, status, run_id, created_by, created_at) VALUES (1, 'RAW', 'CUST', 'BRONZE', 'CUST', 'FULL', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), (SELECT COUNT(*) FROM {{ ref('bronze_customer') }}), 'SUCCESS', '{{ invocation_id }}', CURRENT_USER(), CURRENT_TIMESTAMP());"
 ) }}
 
 -- This is a dummy model that just serves to run the post-hook
