644f250421f4a0af72e634d176ce5ad37110c206
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-13 02:32:16 +0530

    Automated DBT Sync

--- models/si_billing_events.sql
+++ models/si_billing_events.sql
@@ -2,9 +2,7 @@
 -- Transforms bronze billing events data with validation and cleansing
 
 {{ config(
-    materialized='table',
-    pre_hook="INSERT INTO {{ ref('si_process_audit') }} (execution_id, pipeline_name, start_time, status, records_processed, records_successful, records_failed, processing_duration_seconds, source_system, target_system, process_type, load_date, update_date) SELECT '{{ invocation_id }}_si_billing_events', 'si_billing_events', CURRENT_TIMESTAMP, 'RUNNING', 0, 0, 0, 0, 'BRONZE', 'SILVER', 'ETL', CURRENT_DATE, CURRENT_DATE WHERE '{{ this.name }}' != 'si_process_audit'",
-    post_hook="UPDATE {{ ref('si_process_audit') }} SET end_time = CURRENT_TIMESTAMP, status = 'SUCCESS', processing_duration_seconds = DATEDIFF('second', start_time, CURRENT_TIMESTAMP), update_date = CURRENT_DATE WHERE execution_id = '{{ invocation_id }}_si_billing_events' AND '{{ this.name }}' != 'si_process_audit'"
+    materialized='table'
 ) }}
 
 WITH bronze_billing_events AS (
