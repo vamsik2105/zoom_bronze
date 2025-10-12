a17ae42e9d1b168724e5be7386e4d4c36c5895c6
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-12 16:37:06 +0530

    Automated DBT Sync

--- models/bronze/bz_billing_events.sql
+++ models/bronze/bz_billing_events.sql
@@ -18,7 +18,7 @@ WITH source_data AS (
         load_timestamp,
         update_timestamp,
         source_system
-    FROM {{ source('raw_data', 'billing_events') }}
+    FROM {{ source('raw', 'billing_events') }}
 ),
 
 data_quality_checks AS (
