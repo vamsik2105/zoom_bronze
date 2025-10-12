5cd54d462ce2e7220b947077b85540b3e2b1b5a6
vamsi krishna <mkorada@gitam.in>
2025-10-12 20:27:36 +0000

    delete all

--- models/silver/test_model.sql
+++
@@ -1,9 +0,0 @@
--- Simple test model to verify DBT setup
-{{ config(
-    materialized='table'
-) }}
-
-SELECT 
-    'test' as test_column,
-    CURRENT_TIMESTAMP() as created_at,
-    'Zoom_Customer_Analytics' as project_name
