41964677552f0232ac221ebe306cebc5eb19026a
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-13 15:01:10 +0530

    Automated DBT Sync

--- models/bronze/bz_audit_log.sql
+++ models/bronze/bz_audit_log.sql
@@ -8,7 +8,7 @@
 -- This table tracks processing metadata for all bronze layer transformations
 SELECT 
     1 as record_id,
-    'AUDIT_LOG_INIT' as source_table,
+    CAST('AUDIT_LOG_INIT' AS VARCHAR(100)) as source_table,
     CURRENT_TIMESTAMP() as load_timestamp,
     'DBT_SYSTEM' as processed_by,
     0 as processing_time,
