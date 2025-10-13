5284b954c2dd40f46f82be0a81209c318ed49ad0
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-13 15:01:14 +0530

    Automated DBT Sync

--- models/bronze/bz_support_tickets.sql
+++ models/bronze/bz_support_tickets.sql
@@ -16,7 +16,7 @@ WITH source_data AS (
         load_timestamp,
         update_timestamp,
         source_system
-    FROM {{ source('raw_zoom', 'support_tickets') }}
+    FROM {{ source('raw_data', 'support_tickets') }}
 ),
 
 -- Data quality and cleansing layer
