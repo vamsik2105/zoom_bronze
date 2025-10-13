416025cdcf61e609f718365902e5713e5807b5ba
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-13 18:18:51 +0530

    Automated DBT Sync

--- models/silver/si_process_audit.sql
+++ models/silver/si_process_audit.sql
@@ -1,28 +1,24 @@
-{{ config(
-    materialized='table',
-    pre_hook=none,
-    post_hook=none
-) }}
+{{ config(materialized='table') }}
 
 -- Process Audit Table - Created first to support other models
 SELECT 
-    CAST(NULL AS STRING) as execution_id,
-    CAST(NULL AS STRING) as pipeline_name,
-    CAST(NULL AS TIMESTAMP_NTZ) as start_time,
-    CAST(NULL AS TIMESTAMP_NTZ) as end_time,
-    CAST(NULL AS STRING) as status,
+    CAST('init' AS STRING) as execution_id,
+    CAST('initialization' AS STRING) as pipeline_name,
+    CURRENT_TIMESTAMP() as start_time,
+    CURRENT_TIMESTAMP() as end_time,
+    CAST('SUCCESS' AS STRING) as status,
     CAST(NULL AS STRING) as error_message,
-    CAST(NULL AS NUMBER) as records_processed,
-    CAST(NULL AS NUMBER) as records_successful,
-    CAST(NULL AS NUMBER) as records_failed,
-    CAST(NULL AS NUMBER) as processing_duration_seconds,
-    CAST(NULL AS VARCHAR(255)) as source_system,
-    CAST(NULL AS VARCHAR(255)) as target_system,
-    CAST(NULL AS STRING) as process_type,
-    CAST(NULL AS STRING) as user_executed,
-    CAST(NULL AS STRING) as server_name,
+    CAST(0 AS NUMBER) as records_processed,
+    CAST(0 AS NUMBER) as records_successful,
+    CAST(0 AS NUMBER) as records_failed,
+    CAST(0 AS NUMBER) as processing_duration_seconds,
+    CAST('SYSTEM' AS VARCHAR(255)) as source_system,
+    CAST('SILVER' AS VARCHAR(255)) as target_system,
+    CAST('INIT' AS STRING) as process_type,
+    CAST('dbt' AS STRING) as user_executed,
+    CAST('dbt_cloud' AS STRING) as server_name,
     CAST(NULL AS NUMBER) as memory_usage_mb,
     CAST(NULL AS NUMBER) as cpu_usage_percent,
-    CAST(NULL AS DATE) as load_date,
-    CAST(NULL AS DATE) as update_date
+    CURRENT_DATE() as load_date,
+    CURRENT_DATE() as update_date
 WHERE 1=0  -- Empty table structure
