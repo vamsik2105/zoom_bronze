d8c0d748de022990ea622e9dee2a9d1b0d823f02
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-14 19:01:32 +0530

    Automated DBT Sync

--- models/gold/go_process_audit.sql
+++ models/gold/go_process_audit.sql
@@ -1,26 +1,28 @@
 {{ config(
-    materialized='table'
+    materialized='table',
+    pre_hook=none,
+    post_hook=none
 ) }}
 
--- Process Audit Table for Gold Layer
 SELECT 
-    'INIT' AS execution_id,
-    'INITIALIZATION' AS pipeline_name,
-    'DBT_MODEL' AS process_type,
-    CURRENT_TIMESTAMP() AS start_time,
-    CURRENT_TIMESTAMP() AS end_time,
-    'COMPLETED' AS status,
-    NULL AS error_message,
-    0 AS records_processed,
-    0 AS records_successful,
-    0 AS records_failed,
-    0 AS processing_duration_seconds,
-    'SILVER' AS source_system,
-    'GOLD' AS target_system,
-    'DBT_CLOUD' AS user_executed,
-    'DBT_SERVER' AS server_name,
-    NULL AS memory_usage_mb,
-    NULL AS cpu_usage_percent,
-    NULL AS data_volume_gb,
-    CURRENT_DATE() AS load_date,
-    CURRENT_DATE() AS update_date
+    CAST(NULL AS VARCHAR(50)) as execution_id,
+    CAST(NULL AS VARCHAR(255)) as pipeline_name,
+    CAST(NULL AS VARCHAR(100)) as process_type,
+    CAST(NULL AS TIMESTAMP_NTZ) as start_time,
+    CAST(NULL AS TIMESTAMP_NTZ) as end_time,
+    CAST(NULL AS VARCHAR(50)) as status,
+    CAST(NULL AS VARCHAR(2000)) as error_message,
+    CAST(NULL AS NUMBER) as records_processed,
+    CAST(NULL AS NUMBER) as records_successful,
+    CAST(NULL AS NUMBER) as records_failed,
+    CAST(NULL AS NUMBER) as processing_duration_seconds,
+    CAST(NULL AS VARCHAR(100)) as source_system,
+    CAST(NULL AS VARCHAR(100)) as target_system,
+    CAST(NULL AS VARCHAR(100)) as user_executed,
+    CAST(NULL AS VARCHAR(100)) as server_name,
+    CAST(NULL AS NUMBER) as memory_usage_mb,
+    CAST(NULL AS NUMBER(5,2)) as cpu_usage_percent,
+    CAST(NULL AS NUMBER(10,2)) as data_volume_gb,
+    CAST(NULL AS DATE) as load_date,
+    CAST(NULL AS DATE) as update_date
+WHERE 1=0
