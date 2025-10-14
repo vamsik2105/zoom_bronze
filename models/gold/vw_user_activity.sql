c8329e8735ad9ceea2b4ecc9344fff067754e075
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-14 22:00:46 +0530

    Automated DBT Sync

---
+++ models/gold/vw_user_activity.sql
@@ -0,0 +1,19 @@
+{{ config(
+    materialized='view'
+) }}
+
+SELECT 
+    ua.user_id,
+    u.user_name,
+    u.email,
+    u.company as department_name,
+    ua.activity_month,
+    ua.meetings_hosted,
+    ua.meetings_attended,
+    ua.total_hosting_minutes,
+    ua.total_attendance_minutes,
+    ua.average_meeting_quality,
+    u.company as organization_name
+FROM {{ ref('go_monthly_user_activity') }} ua
+JOIN {{ source('silver', 'si_users') }} u ON ua.user_id = u.user_id
+WHERE u.record_status = 'ACTIVE'
