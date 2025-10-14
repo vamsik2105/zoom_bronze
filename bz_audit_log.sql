66e431233ebaed15e9f30bed499b2a6ecd2f98e3
vamsi krishna <mkorada@gitam.in>
2025-10-14 06:46:42 +0000

    delete silver

--- bz_audit_log.sql
+++
@@ -1,2 +0,0 @@
-{{ config(materialized='table') }}
-SELECT 1 as record_id, CAST('INITIAL_LOAD' AS VARCHAR(255)) as source_table, CURRENT_TIMESTAMP() as load_timestamp, 'dbt' as processed_by, 0 as processing_time, 'INITIALIZED' as status WHERE FALSE
--- bz_users.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='table') }}
-WITH source_data AS (SELECT user_id, user_name, email, company, plan_type FROM {{ source('raw_data', 'users') }} WHERE user_id IS NOT NULL)
-SELECT user_id, user_name, email, company, plan_type, CURRENT_TIMESTAMP() as load_timestamp, CURRENT_TIMESTAMP() as update_timestamp, 'ZOOM_PLATFORM' as source_system FROM source_data
