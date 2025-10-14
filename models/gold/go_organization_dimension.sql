632937cadb1a2fe51306d55457f6d55232354fad
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-14 19:01:33 +0530

    Automated DBT Sync

--- models/gold/go_organization_dimension.sql
+++ models/gold/go_organization_dimension.sql
@@ -2,26 +2,24 @@
     materialized='table'
 ) }}
 
--- Gold Organization Dimension Table
--- Note: Since organization table is not available in Silver, creating default organization
-WITH default_organization AS (
+WITH final AS (
     SELECT 
-        {{ dbt_utils.generate_surrogate_key(["'DEFAULT_ORG'"]) }} AS organization_dim_id,
-        'DEFAULT_ORG' AS organization_id,
-        'Default Organization' AS organization_name,
-        'Technology' AS industry_classification,
-        'Medium' AS organization_size,
-        'admin@company.com' AS primary_contact_email,
-        'Default Address' AS billing_address,
-        'Default Manager' AS account_manager_name,
-        CURRENT_DATE() AS contract_start_date,
-        DATEADD('year', 1, CURRENT_DATE()) AS contract_end_date,
-        1000 AS maximum_user_limit,
-        100 AS storage_quota_gb,
-        'Standard' AS security_policy_level,
-        CURRENT_DATE() AS load_date,
-        CURRENT_DATE() AS update_date,
-        'SYSTEM_GENERATED' AS source_system
+        {{ dbt_utils.generate_surrogate_key(['1']) }} as organization_dim_id,
+        'ORG_001' as organization_id,
+        'Default Organization' as organization_name,
+        NULL as industry_classification,
+        NULL as organization_size,
+        NULL as primary_contact_email,
+        NULL as billing_address,
+        NULL as account_manager_name,
+        NULL as contract_start_date,
+        NULL as contract_end_date,
+        NULL as maximum_user_limit,
+        NULL as storage_quota_gb,
+        NULL as security_policy_level,
+        CURRENT_DATE() as load_date,
+        CURRENT_DATE() as update_date,
+        'SYSTEM' as source_system
 )
 
-SELECT * FROM default_organization
+SELECT * FROM final
