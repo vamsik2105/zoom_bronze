58b262f0f189e8f6019122d5c507afbb677e58ad
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-15 21:44:35 +0530

    Automated DBT Sync

---
+++ models/gold/dimensional/go_organization_dimension.sql
@@ -0,0 +1,57 @@
+{{ config(
+    materialized='table',
+    pre_hook="{% if this.name != 'go_process_audit' %}INSERT INTO {{ ref('go_process_audit') }} (process_id, process_name, source_table, target_table, process_status, start_time, end_time, records_processed, error_message, load_date, update_date, source_system) VALUES (UUID_STRING(), 'go_organization_dimension_transform', 'si_users', 'go_organization_dimension', 'STARTED', CURRENT_TIMESTAMP(), NULL, 0, NULL, CURRENT_DATE(), CURRENT_DATE(), 'DBT_TRANSFORM'){% endif %}",
+    post_hook="{% if this.name != 'go_process_audit' %}INSERT INTO {{ ref('go_process_audit') }} (process_id, process_name, source_table, target_table, process_status, start_time, end_time, records_processed, error_message, load_date, update_date, source_system) VALUES (UUID_STRING(), 'go_organization_dimension_transform', 'si_users', 'go_organization_dimension', 'COMPLETED', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), (SELECT COUNT(*) FROM {{ this }}), NULL, CURRENT_DATE(), CURRENT_DATE(), 'DBT_TRANSFORM'){% endif %}"
+) }}
+
+WITH silver_organizations AS (
+    SELECT DISTINCT
+        company,
+        source_system,
+        load_date,
+        update_date
+    FROM {{ ref('si_users') }}
+    WHERE company IS NOT NULL
+      AND record_status = 'ACTIVE'
+      AND data_quality_score >= 0.8
+),
+
+final_transformation AS (
+    SELECT 
+        UUID_STRING() AS organization_dim_id,
+        UUID_STRING() AS organization_id,
+        company AS organization_name,
+        CAST(NULL AS VARCHAR(200)) AS industry_classification,
+        CAST(NULL AS VARCHAR(50)) AS organization_size,
+        CAST(NULL AS VARCHAR(320)) AS primary_contact_email,
+        CAST(NULL AS VARCHAR(1000)) AS billing_address,
+        CAST(NULL AS VARCHAR(255)) AS account_manager_name,
+        CAST(NULL AS DATE) AS contract_start_date,
+        CAST(NULL AS DATE) AS contract_end_date,
+        CAST(NULL AS NUMBER) AS maximum_user_limit,
+        CAST(NULL AS NUMBER) AS storage_quota_gb,
+        CAST(NULL AS VARCHAR(100)) AS security_policy_level,
+        load_date,
+        update_date,
+        source_system
+    FROM silver_organizations
+)
+
+SELECT 
+    organization_dim_id,
+    organization_id,
+    organization_name,
+    industry_classification,
+    organization_size,
+    primary_contact_email,
+    billing_address,
+    account_manager_name,
+    contract_start_date,
+    contract_end_date,
+    maximum_user_limit,
+    storage_quota_gb,
+    security_policy_level,
+    load_date,
+    update_date,
+    source_system
+FROM final_transformation
