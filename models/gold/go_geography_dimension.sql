eb585caa4f5f9860ac4df431a59ff33bf9cb37dc
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-15 18:50:38 +0530

    Automated DBT Sync

---
+++ models/gold/go_geography_dimension.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='table'
+) }}
+
+-- Gold Geography Dimension Table
+-- Creates default geography dimension since geo data not available in Silver
+
+WITH default_geography AS (
+    SELECT 
+        {{ dbt_utils.generate_surrogate_key(['country_code']) }} as geography_dim_id,
+        'US' as country_code,
+        'United States' as country_name,
+        'North America' as region_name,
+        'UTC' as time_zone,
+        'North America' as continent,
+        CURRENT_DATE() as load_date,
+        CURRENT_DATE() as update_date,
+        'DEFAULT' as source_system,
+        CURRENT_TIMESTAMP() as created_at,
+        CURRENT_TIMESTAMP() as updated_at,
+        'SUCCESS' as process_status
+)
+
+SELECT * FROM default_geography
