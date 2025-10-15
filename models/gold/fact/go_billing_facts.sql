40c072c14f5f9e11bcb5693586cfb86cf26a3073
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-15 22:19:32 +0530

    Automated DBT Sync

--- models/gold/fact/go_billing_facts.sql
+++ models/gold/fact/go_billing_facts.sql
@@ -17,7 +17,7 @@ WITH silver_billing_events AS (
         update_date,
         data_quality_score,
         record_status
-    FROM {{ source('silver', 'si_billing_events') }}
+    FROM SILVER.si_billing_events
     WHERE record_status = 'ACTIVE'
 ),
 
@@ -28,7 +28,7 @@ silver_users AS (
         email,
         company,
         plan_type
-    FROM {{ source('silver', 'si_users') }}
+    FROM SILVER.si_users
     WHERE record_status = 'ACTIVE'
 )
 
