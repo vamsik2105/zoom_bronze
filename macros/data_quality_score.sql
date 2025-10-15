8709763b25b22d19220a2a3c155cbd6d4c0ba6b7
vamsi krishna <mkorada@gitam.in>
2025-10-15 07:45:50 +0000

    commit

--- macros/data_quality_score.sql
+++
@@ -1,7 +0,0 @@
-{% macro calculate_data_quality_score(table_alias='') %}
-    CASE 
-        WHEN {{ table_alias }}user_id IS NULL OR {{ table_alias }}user_name IS NULL OR {{ table_alias }}email IS NULL THEN 0.0
-        WHEN NOT REGEXP_LIKE({{ table_alias }}email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') THEN 0.5
-        ELSE 1.0
-    END
-{% endmacro %}
