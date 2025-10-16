9b09954d305c75cdb1300a758fe2901856aa3703
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-16 12:19:12 +0530

    Automated DBT Sync

--- models/gold/fact/go_quality_facts.sql
+++ models/gold/fact/go_quality_facts.sql
@@ -1,51 +1,34 @@
 {{ config(
-    materialized='table',
-    cluster_by=['load_date', 'meeting_id']
+    materialized='table'
 ) }}
 
-WITH participant_quality AS (
-    SELECT 
-        p.meeting_id,
-        p.participant_id,
-        p.join_time,
-        p.leave_time,
-        p.data_quality_score,
-        p.load_date,
-        p.source_system
-    FROM {{ source('silver', 'si_participants') }} p
-    WHERE p.record_status = 'ACTIVE'
-),
-
-final_quality_facts AS (
-    SELECT 
-        CONCAT('QF_', pq.meeting_id, '_', pq.participant_id) as quality_fact_id,
-        pq.meeting_id,
-        pq.participant_id,
-        CONCAT('DC_', pq.participant_id, '_', CURRENT_TIMESTAMP()::STRING) as device_connection_id,
-        ROUND(pq.data_quality_score * 0.8, 2) as audio_quality_score,
-        ROUND(pq.data_quality_score * 0.9, 2) as video_quality_score,
-        ROUND(pq.data_quality_score, 2) as connection_stability_rating,
-        CASE 
-            WHEN pq.data_quality_score > 8 THEN 50
-            WHEN pq.data_quality_score > 6 THEN 100
-            ELSE 200
-        END as latency_ms,
-        CASE 
-            WHEN pq.data_quality_score > 8 THEN 0.01
-            WHEN pq.data_quality_score > 6 THEN 0.05
-            ELSE 0.1
-        END as packet_loss_rate,
-        DATEDIFF('minute', pq.join_time, pq.leave_time) * 2 as bandwidth_utilization,
-        CASE 
-            WHEN pq.data_quality_score > 8 THEN 25.0
-            WHEN pq.data_quality_score > 6 THEN 50.0
-            ELSE 75.0
-        END as cpu_usage_percentage,
-        DATEDIFF('minute', pq.join_time, pq.leave_time) * 10 as memory_usage_mb,
-        pq.load_date,
-        CURRENT_DATE() as update_date,
-        pq.source_system
-    FROM participant_quality pq
-)
-
-SELECT * FROM final_quality_facts
+SELECT 
+    CONCAT('QF_', meeting_id, '_', participant_id) as quality_fact_id,
+    meeting_id,
+    participant_id,
+    CONCAT('DC_', participant_id, '_', CURRENT_TIMESTAMP()::STRING) as device_connection_id,
+    ROUND(data_quality_score * 0.8, 2) as audio_quality_score,
+    ROUND(data_quality_score * 0.9, 2) as video_quality_score,
+    ROUND(data_quality_score, 2) as connection_stability_rating,
+    CASE 
+        WHEN data_quality_score > 8 THEN 50
+        WHEN data_quality_score > 6 THEN 100
+        ELSE 200
+    END as latency_ms,
+    CASE 
+        WHEN data_quality_score > 8 THEN 0.01
+        WHEN data_quality_score > 6 THEN 0.05
+        ELSE 0.1
+    END as packet_loss_rate,
+    DATEDIFF('minute', join_time, leave_time) * 2 as bandwidth_utilization,
+    CASE 
+        WHEN data_quality_score > 8 THEN 25.0
+        WHEN data_quality_score > 6 THEN 50.0
+        ELSE 75.0
+    END as cpu_usage_percentage,
+    DATEDIFF('minute', join_time, leave_time) * 10 as memory_usage_mb,
+    load_date,
+    CURRENT_DATE() as update_date,
+    source_system
+FROM {{ source('silver', 'si_participants') }}
+WHERE record_status = 'ACTIVE'
