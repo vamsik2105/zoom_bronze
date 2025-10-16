3b63d22366a1d335d4f34faa9a897a27c2b86d91
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-16 12:20:05 +0530

    Automated DBT Sync

--- models/gold/fact/go_quality_facts.sql
+++ models/gold/fact/go_quality_facts.sql
@@ -3,32 +3,20 @@
 ) }}
 
 SELECT 
-    CONCAT('QF_', meeting_id, '_', participant_id) as quality_fact_id,
+    participant_id as quality_fact_id,
     meeting_id,
     participant_id,
-    CONCAT('DC_', participant_id, '_', CURRENT_TIMESTAMP()::STRING) as device_connection_id,
-    ROUND(data_quality_score * 0.8, 2) as audio_quality_score,
-    ROUND(data_quality_score * 0.9, 2) as video_quality_score,
-    ROUND(data_quality_score, 2) as connection_stability_rating,
-    CASE 
-        WHEN data_quality_score > 8 THEN 50
-        WHEN data_quality_score > 6 THEN 100
-        ELSE 200
-    END as latency_ms,
-    CASE 
-        WHEN data_quality_score > 8 THEN 0.01
-        WHEN data_quality_score > 6 THEN 0.05
-        ELSE 0.1
-    END as packet_loss_rate,
-    DATEDIFF('minute', join_time, leave_time) * 2 as bandwidth_utilization,
-    CASE 
-        WHEN data_quality_score > 8 THEN 25.0
-        WHEN data_quality_score > 6 THEN 50.0
-        ELSE 75.0
-    END as cpu_usage_percentage,
-    DATEDIFF('minute', join_time, leave_time) * 10 as memory_usage_mb,
+    participant_id as device_connection_id,
+    data_quality_score as audio_quality_score,
+    data_quality_score as video_quality_score,
+    data_quality_score as connection_stability_rating,
+    50 as latency_ms,
+    0.01 as packet_loss_rate,
+    100 as bandwidth_utilization,
+    25.0 as cpu_usage_percentage,
+    512 as memory_usage_mb,
     load_date,
     CURRENT_DATE() as update_date,
     source_system
-FROM {{ source('silver', 'si_participants') }}
+FROM SILVER.si_participants
 WHERE record_status = 'ACTIVE'
