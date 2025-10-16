ae60232d61c5b7d93b0d9d83e8112a8eaf00298f
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-16 11:06:02 +0530

    Automated DBT Sync

--- models/gold/go_process_audit.sql
+++ models/gold/go_process_audit.sql
@@ -3,14 +3,13 @@
 ) }}
 
 SELECT 
-    'INIT'::VARCHAR(255) as process_id,
-    'GOLD_FACT_LOAD'::VARCHAR(255) as process_name,
-    'SILVER_TABLES'::VARCHAR(255) as source_table,
-    'GOLD_FACTS'::VARCHAR(255) as target_table,
-    'INITIALIZED'::VARCHAR(50) as process_status,
+    'INIT' as process_id,
+    'GOLD_FACT_LOAD' as process_name,
+    'SILVER_TABLES' as source_table,
+    'GOLD_FACTS' as target_table,
+    'INITIALIZED' as process_status,
     CURRENT_TIMESTAMP() as start_time,
-    NULL::TIMESTAMP_NTZ as end_time,
+    CURRENT_TIMESTAMP() as end_time,
     0 as records_processed,
-    NULL::VARCHAR(500) as error_message,
+    'Initial setup' as error_message,
     CURRENT_DATE() as load_date
-WHERE 1=0
