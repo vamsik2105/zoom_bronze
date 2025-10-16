68c52cac81b9860e30003f4a6231cb2756bdfc22
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-16 10:28:05 +0530

    Automated DBT Sync

--- models/gold/go_process_audit.sql
+++ models/gold/go_process_audit.sql
@@ -1,7 +1,5 @@
 {{ config(
-    materialized='table',
-    pre_hook="INSERT INTO {{ this }} (process_id, process_name, source_table, target_table, process_status, start_time, end_time, records_processed, error_message, created_at, updated_at) VALUES (UUID_STRING(), 'gold_transformation', 'multiple_silver_tables', 'go_process_audit', 'STARTED', CURRENT_TIMESTAMP(), NULL, 0, NULL, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()) WHERE '{{ this.name }}' != 'go_process_audit'",
-    post_hook="UPDATE {{ this }} SET process_status = 'COMPLETED', end_time = CURRENT_TIMESTAMP(), updated_at = CURRENT_TIMESTAMP() WHERE process_status = 'STARTED' AND target_table != 'go_process_audit'"
+    materialized='table'
 ) }}
 
 SELECT 
@@ -13,6 +11,6 @@ SELECT
     CURRENT_TIMESTAMP() as start_time,
     CURRENT_TIMESTAMP() as end_time,
     0 as records_processed,
-    NULL as error_message,
+    CAST(NULL AS VARCHAR(255)) as error_message,
     CURRENT_TIMESTAMP() as created_at,
     CURRENT_TIMESTAMP() as updated_at
