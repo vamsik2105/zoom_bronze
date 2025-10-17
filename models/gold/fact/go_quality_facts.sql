a1bba0d26299a7cd2df1f400aa8d12285469b8a3
vamsik2105 <vamsikrish12103@gmail.com>
2025-10-17 21:54:45 +0530

    Automated DBT Sync

---
+++ models/gold/fact/go_quality_facts.sql
@@ -0,0 +1,49 @@
+{{ config(
+    materialized='table',
+    cluster_by=['meeting_id'],
+    pre_hook="INSERT INTO {{ ref('go_process_audit') }} (execution_id, pipeline_name, process_type, start_time, status, source_system, target_system, user_executed) VALUES (CONCAT('EXEC_', CURRENT_TIMESTAMP()::STRING), 'go_quality_facts', 'FACT_LOAD', CURRENT_TIMESTAMP(), 'STARTED', 'SILVER', 'GOLD', CURRENT_USER()) WHERE '{{ this.name }}' != 'go_process_audit'",
+    post_hook="UPDATE {{ ref('go_process_audit') }} SET end_time = CURRENT_TIMESTAMP(), status = 'COMPLETED', records_processed = (SELECT COUNT(*) FROM {{ this }}) WHERE pipeline_name = 'go_quality_facts' AND status = 'STARTED' AND '{{ this.name }}' != 'go_process_audit'"
+) }}
+
+WITH participant_quality AS (
+    SELECT 
+        p.meeting_id,
+        p.participant_id,
+        p.data_quality_score,
+        p.join_time,
+        p.leave_time,
+        p.load_date,
+        p.source_system
+    FROM {{ ref('si_participants') }} p
+    WHERE p.record_status = 'ACTIVE'
+)
+
+SELECT 
+    CONCAT('QF_', pq.meeting_id, '_', pq.participant_id) as quality_fact_id,
+    pq.meeting_id,
+    pq.participant_id,
+    CONCAT('DC_', pq.participant_id, '_', CURRENT_TIMESTAMP()::STRING) as device_connection_id,
+    ROUND(pq.data_quality_score * 0.8, 2) as audio_quality_score,
+    ROUND(pq.data_quality_score * 0.9, 2) as video_quality_score,
+    ROUND(pq.data_quality_score, 2) as connection_stability_rating,
+    CASE 
+        WHEN pq.data_quality_score > 8 THEN 50
+        WHEN pq.data_quality_score > 6 THEN 100
+        ELSE 200
+    END as latency_ms,
+    CASE 
+        WHEN pq.data_quality_score > 8 THEN 0.01
+        WHEN pq.data_quality_score > 6 THEN 0.05
+        ELSE 0.1
+    END as packet_loss_rate,
+    DATEDIFF('minute', pq.join_time, pq.leave_time) * 2 as bandwidth_utilization,
+    CASE 
+        WHEN pq.data_quality_score > 8 THEN 25.0
+        WHEN pq.data_quality_score > 6 THEN 50.0
+        ELSE 75.0
+    END as cpu_usage_percentage,
+    DATEDIFF('minute', pq.join_time, pq.leave_time) * 10 as memory_usage_mb,
+    pq.load_date,
+    CURRENT_DATE() as update_date,
+    pq.source_system
+FROM participant_quality pq
